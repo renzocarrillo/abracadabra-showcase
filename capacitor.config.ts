4873a64fecf143084f9dd5148d4505863abc32b0
renzocarrillo <122653067+renzocarrillo@users.noreply.github.com>
2025-12-18 18:50:54 -0500

    Update Capacitor config with new app details

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,18 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.1a4e5408292648ef917cc41f1b20d434',
-  appName: 'warehouse-whisperer-13',
+  appId: 'com.renzocarrillo.abracadabra',
+  appName: 'abracadabra-showcase',
   webDir: 'dist',
-  server: {
-    url: 'https://1a4e5408-2926-48ef-917c-c41f1b20d434.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 0,
-    },
-  },
 };
 
 export default config;
